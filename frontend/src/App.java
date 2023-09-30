b4f44ec0f625f497a6f31b25528c88888bf44adc
Jack Willis <jackwillis517@gmail.com>
2023-09-29 23:19:30 -0400

    Created a basic display window

--- frontend/src/App.java
+++ frontend/src/App.java
@@ -1,5 +1,27 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    Stage window;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("My First JavaFX Project");
+
+        Button button = new Button("Button");
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(button);
+        Scene scene = new Scene(layout, 600, 600);
+        window.setScene(scene);
+        window.show();
     }
 }
